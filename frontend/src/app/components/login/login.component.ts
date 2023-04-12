2d2076531dbe03b8723c06f31b02a981f5823909
Cleiton Braga <cleitonbraga56@gmail.com>
2023-04-12 16:42:25 -0300

    Add initial logic of 'login' page

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormControl, FormGroup } from '@angular/forms';
+
+import { User } from 'src/app/models/User';
+import { RequestResponseVM } from 'src/app/models/ResponseRequestVM';
+
+import { LoginService } from './login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +14,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public formGroup!: FormGroup;
+
+  constructor(
+    private router: Router,
+    private loginService: LoginService
+  ) { }
 
   ngOnInit(): void {
+    this.setInitialForm();
   }
 
+  private setInitialForm(): void {
+    this.formGroup = new FormGroup({
+      name:     new FormControl(null),
+      password: new FormControl(null),
+    });
+  }
+
+  public navigateTo(path: string): void {
+    this.router.navigateByUrl(path);
+  }
+
+  public onSubmit(): void {
+    this.loginService.login(this.formGroup.getRawValue() as User).subscribe({
+      next: (userData: RequestResponseVM) => {
+        alert(userData.message);
+        localStorage.setItem('user', JSON.stringify(JSON.parse(JSON.stringify(userData.data))));
+        this.navigateTo('profile');
+      },
+      error: (error: any) => {
+        alert(error.error.message);
+      }
+    });
+  }
 }
--- frontend/src/app/components/login/login.service.ts
+++ frontend/src/app/components/login/login.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { environment } from 'src/environments/environment';
+
+import { User } from 'src/app/models/User';
+import { RequestResponseVM } from 'src/app/models/ResponseRequestVM';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  private readonly BASE_URL: string = `${environment.server_url}/user`;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  public login(bodyParams: Partial<User>): Observable<any> {
+    const url: string = `${this.BASE_URL}/login`;
+
+    return this.http.post<RequestResponseVM>(url, bodyParams);
+  }
 }
