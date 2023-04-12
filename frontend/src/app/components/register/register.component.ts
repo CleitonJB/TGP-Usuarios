602318f9b15d89545737612c8a6dd8aece284033
Cleiton Braga <cleitonbraga56@gmail.com>
2023-04-12 16:43:27 -0300

    Add initial logic of 'register' page

--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormControl, FormGroup } from '@angular/forms';
+
+import { User } from 'src/app/models/User';
+import { RequestResponseVM } from 'src/app/models/ResponseRequestVM';
+
+import { RegisterService } from './register.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +14,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  public formGroup!: FormGroup;
+  
+  constructor(
+    private router: Router,
+    private registerService: RegisterService
+  ) { }
 
   ngOnInit(): void {
+    this.setInitialForm();
   }
 
+  private setInitialForm(): void {
+    this.formGroup = new FormGroup({
+      id:       new FormControl(1),
+      name:     new FormControl(null),
+      email:    new FormControl(null),
+      password: new FormControl(null),
+      role:     new FormControl(1)
+    });
+  }
+
+  public navigateTo(path: string): void {
+    this.router.navigateByUrl(path);
+  }
+
+  public onSubmit(): void {
+    this.registerService.register(this.formGroup.getRawValue() as User).subscribe({
+      next: (userData: RequestResponseVM) => {
+        alert(userData.message);
+        localStorage.setItem('user', JSON.stringify(JSON.parse(JSON.stringify(userData.data)))); 
+        this.navigateTo('profile');
+      },
+      error: (error: any) => {
+        alert(error.error.message);
+      }
+    });
+  }
 }
--- frontend/src/app/components/register/register.service.ts
+++ frontend/src/app/components/register/register.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { environment } from 'src/environments/environment';
+
+import { User } from 'src/app/models/User';
+import { RequestResponseVM } from 'src/app/models/ResponseRequestVM';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
 
-  constructor() { }
+  private readonly BASE_URL: string = `${environment.server_url}/user`;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  public register(bodyParams: User): Observable<any> {
+    const url: string = `${this.BASE_URL}/register`;
+
+    return this.http.post<RequestResponseVM>(url, bodyParams);
+  }
 }
