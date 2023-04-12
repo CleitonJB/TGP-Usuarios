e9e9e6bb524ec1cd733434174415dd14967bee06
Cleiton Braga <cleitonbraga56@gmail.com>
2023-04-12 14:45:11 -0300

    Install @ng-bootstrap/ng-bootstrap

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -36,7 +37,8 @@ import { AutorizacaoComponent } from './components/autorizacao/autorizacao.compo
     MatToolbarModule,
     MatIconModule,
     MatSidenavModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
