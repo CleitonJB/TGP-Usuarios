2b728fec2c54cd853ca6b52e89a8cbbcfcdcada6
Cleiton Braga <cleitonbraga56@gmail.com>
2023-04-12 01:18:04 -0300

    Create navigation logic on side menu

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public showFiller: boolean = false;
+
+  constructor(private router: Router) {}
+
+  public navigateTo(path: string): void {
+    this.router.navigateByUrl(path);
+  }
 }
