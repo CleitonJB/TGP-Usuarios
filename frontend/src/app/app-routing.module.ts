1ca876753255b8a1b8c612f1202524369deb1019
Cleiton Braga <cleitonbraga56@gmail.com>
2023-04-12 14:43:09 -0300

    Create 'login' and 'register' routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,15 +2,29 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { RoleComponent } from './components/role/role.component';
+import { LoginComponent } from './components/login/login.component';
 import { ProfileComponent } from './components/profile/profile.component';
-import { FuncionalidadeComponent } from './components/funcionalidade/funcionalidade.component';
+import { RegisterComponent } from './components/register/register.component';
 import { AutorizacaoComponent } from './components/autorizacao/autorizacao.component';
+import { FuncionalidadeComponent } from './components/funcionalidade/funcionalidade.component';
 
 const routes: Routes = [
+  // {
+  //   path: '**',
+  //   redirectTo: Page404Compoenent
+  // },
   {
     path: '',
     redirectTo: 'profile',
     pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
   },
   {
     path: 'profile',
