77a9f903a781bfdff13f347b9d73aa3adde30f96
Cleiton Braga <cleitonbraga56@gmail.com>
2023-04-12 16:40:56 -0300

    Add initial logic of 'App' (main) component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,15 +1,32 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { User } from './models/User';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   public showFiller: boolean = false;
+  public currentUser!: User;
 
   constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.getUser();
+  }
+
+  private getUser(): void{
+    this.currentUser = JSON.parse(localStorage.getItem('user') as string);
+  }
+
+  public logout(): void {
+    localStorage.removeItem('user');
+    this.navigateTo('login');
+  }
 
   public navigateTo(path: string): void {
     this.router.navigateByUrl(path);
