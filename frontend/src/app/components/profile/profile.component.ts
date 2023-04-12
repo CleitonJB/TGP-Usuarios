fa3501546b78968368594853333a0b2f3a413fdd
Cleiton Braga <cleitonbraga56@gmail.com>
2023-04-12 16:43:50 -0300

    Add initial logic of 'profile' page

--- frontend/src/app/components/profile/profile.component.ts
+++ frontend/src/app/components/profile/profile.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormControl, FormGroup } from '@angular/forms';
+
+import { User } from 'src/app/models/User';
+import { RequestResponseVM } from 'src/app/models/ResponseRequestVM';
+
+import { ProfileService } from './profile.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +14,76 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  public currentUser!: User;
+  public formGroup!: FormGroup;
+
+  constructor(
+    private router: Router,
+    private profileService: ProfileService
+  ) { }
 
   ngOnInit(): void {
+    this.setInitialForm();
+    this.getUser();
   }
 
+  private setInitialForm(): void {
+    this.formGroup = new FormGroup({
+      id:        new FormControl(null),
+      name:      new FormControl(null),
+      namefull:  new FormControl(null),
+      email:     new FormControl(null),
+      password:  new FormControl(null),
+      role:      new FormControl(null),
+      cellphone: new FormControl(null),
+    });
+  }
+
+  private getUser(): void {
+    const user: User = Object.assign({}, JSON.parse(localStorage.getItem('user') as string));
+    delete user.id;
+    delete user.email;
+    delete user.role;
+
+    this.profileService.get(user).subscribe({
+      next: (userData: RequestResponseVM) => {
+        console.warn(userData);
+        alert(userData.message);
+        this.currentUser = userData.data;
+        this.formGroup.patchValue(userData.data);
+      },
+      error: (error: any) => {
+        console.warn(error);
+        alert(error.error.message);
+        console.error("Erro ao buscar dados do usuário: ", error);
+      }
+    });
+  }
+
+  public updateUser(): void {
+    this.profileService.update(this.formGroup.getRawValue()).subscribe({
+      next: (userData: RequestResponseVM) => {
+        alert(userData.message);
+        this.formGroup.patchValue(userData);
+      },
+      error: (error: any) => {
+        alert(error.error.message);
+        console.error("Erro ao buscar dados do usuário: ", error);
+      }
+    });
+  }
+
+  public deleteUser(): void {
+    this.profileService.delete(this.formGroup.getRawValue().id).subscribe({
+      next: (userData: RequestResponseVM) => {
+        alert(userData.message);
+        localStorage.removeItem('user');
+        this.router.navigateByUrl('login');
+      },
+      error: (error: any) => {
+        alert(error.error.message);
+        console.error("Erro ao buscar dados do usuário: ", error);
+      }
+    });
+  }
 }
--- frontend/src/app/components/profile/profile.service.ts
+++ frontend/src/app/components/profile/profile.service.ts
@@ -1,9 +1,38 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { environment } from 'src/environments/environment';
+
+import { User } from 'src/app/models/User';
+import { RequestResponseVM } from 'src/app/models/ResponseRequestVM';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  constructor() { }
+  private readonly BASE_URL: string = `${environment.server_url}/user`;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  public get(bodyParams: Partial<User>): Observable<any> {
+    const url: string = `${this.BASE_URL}/login`;
+
+    return this.http.post<RequestResponseVM>(url, bodyParams);
+  }
+
+  public update(bodyParams: User): Observable<any> {
+    const url: string = `${this.BASE_URL}/update`;
+
+    return this.http.put<RequestResponseVM>(url, bodyParams);
+  }
+
+  public delete(UserID: number): Observable<any> {
+    const url: string = `${this.BASE_URL}/delete/${UserID}`;
+
+    return this.http.delete<RequestResponseVM>(url);
+  }
 }
