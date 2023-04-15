1de79335ceda2663b287e6efc0bcf97fa6bed3d4
Cleiton Braga <cleitonbraga56@gmail.com>
2023-04-15 14:30:43 -0300

    Create localStorage service

---
+++ frontend/src/app/core/services/local-storage/local-storage.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+
+import { User } from 'src/app/models/User';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalStorageService {
+
+  private readonly USER_KEY: string = 'user';
+
+  constructor() { }
+
+  //* Default
+  private setDataAtStorage(key: string, value: any): void {
+    localStorage.setItem(key, JSON.stringify(JSON.parse(JSON.stringify(value))));
+  }
+
+  private getStorageData(key: string): string | null {
+    return localStorage.getItem(key);
+  }
+
+  private removeDataAtStorage(key: string): void {
+    localStorage.removeItem(key);
+  }
+
+  //* User
+  public setUser(user: User): void {
+    this.setDataAtStorage(this.USER_KEY, user);
+  }
+
+  public getUser(): User | null {
+    return this.getStorageData(this.USER_KEY) as User | null;
+  }
+
+  public removeUser(): void {
+    this.removeDataAtStorage(this.USER_KEY);
+  }
+}
