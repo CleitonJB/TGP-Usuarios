65507d333d5bf04a0fc31f3d7ffd27a9657d3bc5
Cleiton Braga <cleitonbraga56@gmail.com>
2023-04-16 23:48:55 -0300

    Add role page CRUD logic

---
+++ frontend/src/app/pages/role/role-detail/role-detail.component.ts
@@ -0,0 +1,132 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { Role } from 'src/app/models/Role';
+import { RequestResponseVM } from 'src/app/models/ResponseRequestVM';
+import { DetailData, DetailMode } from 'src/app/models/DetailVM';
+
+import { RoleService } from '../role.service';
+
+@Component({
+  selector: 'app-role-detail',
+  templateUrl: './role-detail.component.html',
+  styleUrls: ['./role-detail.component.scss']
+})
+export class RoleDetailComponent implements OnInit {
+  
+  public formGroup!: FormGroup;
+
+  constructor(
+    private dialogRef: MatDialogRef<RoleDetailComponent>,
+    private roleService: RoleService,
+    @Inject(MAT_DIALOG_DATA) public detailData: DetailData,
+  ) { }
+
+  ngOnInit(): void {
+    this.setInitialForm();
+    this.populateData();
+  }
+
+  private setInitialForm(): void {
+    this.formGroup = new FormGroup({
+      id:           new FormControl({ value: null, disabled: true }),
+      description:  new FormControl(null, Validators.required),
+    });
+  }
+
+  private populateData(): void {
+    switch(this.detailData.mode) {
+      case DetailMode.Add:
+      break;
+
+      case DetailMode.Update:
+      break;
+
+      case DetailMode.Delete:
+        this.formGroup.disable();
+      break;
+
+      case DetailMode.View:
+      break;
+    }
+
+    this.formGroup.patchValue(this.detailData.data);
+  }
+
+  public onSubmit(): void {
+    switch(this.detailData.mode) {
+      case DetailMode.Add:
+        this.onCreate();
+      break;
+
+      case DetailMode.Update:
+        this.onUpdate();
+      break;
+
+      case DetailMode.Delete:
+        this.onDelete();
+      break;
+
+      case DetailMode.View:
+        //!
+      break;
+    }
+  }
+
+  private onCreate(): void {
+    const newRole: Role = {
+      id: this.getNextID(),
+      description: this.formGroup.getRawValue().description,
+    }
+
+    this.roleService.create(newRole).subscribe({
+      next: (data: RequestResponseVM) => {
+        alert(data.message);
+        this.onClose();
+      },
+      error: (error: any) => {
+        console.error(error.error.message);
+      }
+    });
+  }
+
+  private onUpdate(): void {
+    this.roleService.update(this.formGroup.getRawValue()).subscribe({
+      next: (data: RequestResponseVM) => {
+        alert(data.message);
+        this.onClose();
+      },
+      error: (error: any) => {
+        console.error(error.error.message);
+      }
+    });
+  }
+
+  private onDelete(): void {
+    this.roleService.delete(this.formGroup.getRawValue().id).subscribe({
+      next: (data: RequestResponseVM) => {
+        alert(data.message);
+        this.onClose();
+      },
+      error: (error: any) => {
+        console.error(error.error.message);
+      }
+    });
+  }
+
+  public onClose(): void {
+    this.dialogRef.close(null);
+  }
+
+  //*
+  private getNextID(): number {
+    console.log(this.detailData.lastData);
+
+    if(this.detailData.lastData) {
+      return Number(this.detailData.lastData.id) + 1;
+    } else {
+      return 1;
+    }
+  }
+}
--- frontend/src/app/pages/role/role.component.ts
+++ frontend/src/app/pages/role/role.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { DetailMode } from 'src/app/models/DetailVM';
+import { Role, RolesID } from '../../models/Role';
+import { RequestResponseVM } from 'src/app/models/ResponseRequestVM';
+
+import { User } from 'src/app/models/User';
+import { RoleService } from './role.service';
+import { UserService } from 'src/app/core/services/user/user.service';
+import { DialogService } from 'src/app/core/services/dialog/dialog.service';
 
 @Component({
   selector: 'app-role',
@@ -6,10 +16,106 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./role.component.scss']
 })
 export class RoleComponent implements OnInit {
+  public ROLES = RolesID;
+  
+  displayedColumns: string[] = ['actions', 'id', 'description'];
+  dataSource: any = null; //!Role[] | null = [];
 
-  constructor() { }
+  private roleSubscription: Subscription | null = null;
+
+  public currentUser!: User | null;
+  private userSubscription: Subscription | null = null;
+
+  constructor(
+    private roleService: RoleService,
+    private userService: UserService,
+    private dialogService: DialogService
+  ) { }
 
   ngOnInit(): void {
+    this.getRoles();
+    this.getUser();
   }
 
+  ngOnDestroy(): void {
+    if(this.roleSubscription) this.roleSubscription.unsubscribe();
+    if(this.userSubscription) this.userSubscription.unsubscribe();
+  }
+
+  private getRoles(): void {
+    this.roleSubscription = this.roleService.get().subscribe({
+      next: (rolesData: RequestResponseVM) => {
+        alert(rolesData.message);
+        this.dataSource = rolesData.data;
+      },
+      error: (error: any) => {
+        alert(error.error.message);
+        console.error("Erro ao buscar roles: ", error);
+      }
+    });
+  }
+
+  private getUser(): void {
+    this.userSubscription = this.userService.getUser().subscribe({
+      next: (userData: User | null) => {
+        this.currentUser = userData;
+      },
+      error: (error: any) => {
+        console.error("Erro ao obter os dados do usuário: ", error);
+      }
+    });
+  }
+  
+  public onAddRole(): void {
+    this.dialogService.openRoleDialog({
+      mode: DetailMode.Add,
+      data: null,
+      lastData: this.dataSource[this.dataSource.length - 1]
+    }).subscribe({
+      next: (data: any) => {
+        this.getRoles();
+      },
+      error: (error: any) => {
+        console.error(error);
+      }
+    });
+  }
+
+  public onEditRole(role: Role): void {
+    this.updateRoles(role);
+  }
+
+  private updateRoles(role: Role): void {
+    this.dialogService.openRoleDialog({
+      mode: DetailMode.Update,
+      data: role,
+      lastData: this.dataSource[this.dataSource.length - 1]
+    }).subscribe({
+      next: (data: any) => {
+        this.getRoles();
+      },
+      error: (error: any) => {
+        console.error(error);
+      }
+    });
+  }
+
+  public onDeleteRole(role: Role): void {
+    this.deleteRoles(role);
+  }
+
+  private deleteRoles(role: Role): void {
+    this.dialogService.openRoleDialog({
+      mode: DetailMode.Delete,
+      data: role,
+      lastData: this.dataSource[this.dataSource.length - 1]
+    }).subscribe({
+      next: (data: any) => {
+        this.getRoles();
+      },
+      error: (error: any) => {
+        console.error(error);
+      }
+    });
+  }
 }
